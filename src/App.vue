8412d3245f4995bbf517e832ce258edc999ba20c
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-06-13 22:52:49 -0400

    slots

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ACard>
+      <h2 slot="header">My Card <strong>Header</strong></h2>
+      Aquí va contenido
+    </ACard>
+    <ACard>
+      Aquí va contenido
+    </ACard>
+  </div>
+</template>
+
+<script>
+import ACard from './components/ACard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ACard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ACard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="header" v-if="this.$slots.header">
+        <slot name="header"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "ACard",
+};
+</script>
+
+<style scoped>
+.card {
+  background: #666;
+  color: white;
+  border-radius: 6px;
+  padding: 1rem;
+}
+
+.header {
+    border-bottom: 1px solid #fff;
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
+}
+</style>
